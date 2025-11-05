8d2115d6c6025a5448a3044b3c7797ed02c046e7
Jordan Pratt <jpratt@mtroyal.ca>
2025-11-05 14:53:57 -0700

    Ready to share.

--- clean.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item -Path .\logs\apache2\*.log -Force
-New-Item .\logs\apache2\.gitkeep
-
-Remove-Item -Path .\logs\xdebug\*.log -Force
-New-Item .\logs\xdebug\.gitkeep
-
-docker compose stop
-docker compose rm -f
-docker system prune -a --volumes -f
